62b0f321a60c243eb6b14c35a59c578b69921efb
Rita Tang <doggymint@gmail.com>
2023-08-30 22:17:43 -0400

    add fetchMyIP, fetchCoordsByIP functions

---
+++ index.js
@@ -0,0 +1,23 @@
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+
+/*
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned IP:", ip);
+});
+*/
+
+/*
+fetchCoordsByIP("42", (error, data) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned geo coordinates:", data);
+});
+*/
---
+++ iss.js
@@ -0,0 +1,46 @@
+// This file contains most of the logic for fetching the data from each API endpoint.
+const request = require("request");
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = (callback) => {
+  // use request to fetch IP address from JSON API
+  request("https://api.ipify.org?format=json", (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const data = JSON.parse(body);
+    callback(null, data.ip);
+  });
+};
+
+const fetchCoordsByIP = (ip, callback) => {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+
+    if (!data.success) {
+      const message = `Success status was ${data.success}. Server message says: ${data.message} when fetching for IP ${data.ip}`;
+      callback(message, null);
+      return;
+    }
+    const { latitude, longitude } = data;
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
